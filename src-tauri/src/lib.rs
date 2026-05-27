0a6eb3f6dace2f4c338731f37d40659dc38b61fe
prathamreet <prathamreet@gmail.com>
2026-05-27 14:12:44 +0530

    feat(tauri): add and register tauri-plugin-updater and tauri-plugin-process

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -38,6 +38,8 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .manage(InitialFileState(Mutex::new(None)))
         .invoke_handler(tauri::generate_handler![read_file, write_file, get_initial_file])
         .setup(|app| {
